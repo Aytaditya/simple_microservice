05d0610de6e568193b5b0b634d3fafe9d68d3a03
Aditya Aryan <adityaaryan531@gmail.com>
2025-11-30 20:33:46 +0530

    user registration

--- users/app.js
+++ users/app.js
@@ -4,7 +4,11 @@ const app=express();
 const http=require('http');
 const userRouter=require('./routes/user.routes')
 const connectDB=require('./db/db')
+const cookieParser=require('cookie-parser')
 
+app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cookieParser()); // Middleware to parse cookies
+app.use(express.urlencoded({extended:true})); // Middleware to parse URL-encoded request bodies
 app.use('/',userRouter)
 
 server=http.createServer(app);
--- users/controllers/user.controller.js
+++ users/controllers/user.controller.js
@@ -1,13 +1,18 @@
 const userModel=require('../models/user.model')
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
+const dotenv=require('dotenv')
+dotenv.config();
+
 
 const jwt_secret="aditya"
+
 
 module.exports.register= async(req,res)=>{
     try {
         const {name,email,password}=req.body;
-        const user=userModel.findOne({email});
+        console.log(name,email,password);
+        const user=await userModel.findOne({email});
         if(user){
             return res.status(400).json({message:"User already exists"});
         }
@@ -23,9 +28,14 @@ module.exports.register= async(req,res)=>{
             jwt_secret,
             {expiresIn:'1h'}
         );
-        res.status(200).json({message:"User registered successfully", token});
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:false, // Set to true if using HTTPS
+            maxAge: 3600000 // 1 hour in milliseconds
+        });
+        return res.status(201).json({message:"User registered successfully", token});
     } catch (error) {
         console.log("Error in user registration:",error);
-        res.status(500).json({message:"Internal server error"});
+        return res.status(500).json({message:"Internal server error"});
     }
 }
